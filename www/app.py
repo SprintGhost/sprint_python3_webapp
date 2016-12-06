d77a3d91a21cb4c59c2bd3d6e19fcd6817373533
SprintGhost <18240069963@163.com>
2016-12-06 17:10:59 +0800

    搭建orm框架

--- www/app.py
+++ www/app.py
@@ -9,20 +9,19 @@ import logging;logging.basicConfig(level=logging.INFO)
 
 import asyncio,os,json,time
 from datetime import datetime
-
 from aiohttp import web
-
+from orm import Model, StringField, IntegerField
 def index(request):
     return web.Response(body=b'<h1>Sprint</h1>',content_type='text/html',charset='UTF-8')
 #如果不加后面的content_type='text/html',charset='UTF-8'会因编码问题导致显示的为下载文件
 
-@asyncio.coroutine
-def init(loop):
+async def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET','/',index)
-    srv = yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
+    srv = await loop.create_server(app.make_handler(),'127.0.0.1',9000)
     logging.info('sever started at http://127.0.0.1:9000...')
     return srv
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
